1d03bc10ace0cae2a706428ae3d8a17d3ba96bb5
Ilija Afanasyev <ia@fino.digital>
2021-06-10 08:56:35 +0200

    Refractor, add comments, add e, phiN comparison

--- rsa/service.go
+++ rsa/service.go
@@ -1,9 +1,11 @@
 package rsa
 
 import (
+	"crypto/rand"
 	"math/big"
 )
 
+// Service represents an encryption technique with encryption/decryption capability
 type Service interface {
 	EncryptMessage([]rune) []*big.Int
 	DecryptMessage([]*big.Int) []rune
@@ -14,6 +16,7 @@ type service struct {
 	key2 *Key
 }
 
+// NewService generates an new rsa instance with keys
 func NewService() Service {
 	key1, key2, err := generatePrivateAndPublicKey()
 	if err != nil {
@@ -42,4 +45,85 @@ func (s *service) DecryptMessage(encryptedMessage []*big.Int) []rune {
 	}
 
 	return decryptedMessage
+}
+
+func generatePrivateAndPublicKey() (*Key, *Key, error) {
+	var err error
+	var p, q, e *big.Int
+	n, d := big.NewInt(0), big.NewInt(0)
+
+	p, q, err = choosePrimes()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	n.Mul(p, q) // multiply primes
+
+	phiN := eulersTotient(p, q)
+
+	e, err = chooseSmallerCoprimeNumber(phiN)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	d.ModInverse(e, phiN)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &Key{Number: e, Mod: n}, &Key{Number: d, Mod: n}, nil
+
+}
+
+func choosePrimes() (*big.Int, *big.Int, error) {
+	const msg = "p or q can not be represented as int64: try to choose smaller numbers"
+	p, err1 := rand.Prime(rand.Reader, 2000)
+	q, err2 := rand.Prime(rand.Reader, 2000)
+
+	if err1 != nil {
+		return nil, nil, err1
+	}
+	if err2 != nil {
+		return nil, nil, err2
+	}
+
+	return p, q, nil
+}
+
+func eulersTotient(p *big.Int, q *big.Int) *big.Int {
+	var pMinusOne, qMinusOne, product big.Int
+
+	one := big.NewInt(1)
+
+	return product.Mul(pMinusOne.Sub(p, one), qMinusOne.Sub(q, one))
+}
+
+func chooseSmallerCoprimeNumber(phiN *big.Int) (*big.Int, error) {
+
+	e, err := rand.Prime(rand.Reader, 2000)
+	if err != nil {
+		return nil, err
+	}
+
+	if e.Cmp(phiN) == -1 {
+		return e, nil
+	}
+
+	e, err = chooseSmallerCoprimeNumber(phiN)
+	if err != nil {
+		return e, nil
+	}
+
+	return e, nil
+}
+
+func encryptRune(r rune, key *Key) *big.Int {
+	message := big.NewInt(int64(r))
+	return message.Exp(message, key.Number, key.Mod)
+}
+
+func decryptRune(encryptedRune *big.Int, key *Key) rune {
+	encryptedRune = encryptedRune.Exp(encryptedRune, key.Number, key.Mod)
+
+	return rune(encryptedRune.Int64())
 }
